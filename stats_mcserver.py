1e22a83aff5f04928a3fbc1ad29813233df782a8
Timo <Timobrunn@gmail.com>
2022-05-14 14:12:42 +0200

    Update stats_mcserver.py
    Handle the expection so the template can recognize it as offline

--- stats_mcserver.py
+++ stats_mcserver.py
@@ -9,14 +9,19 @@ import sys
 
 # Get server instance
 server = JavaServer(sys.argv[2], int(sys.argv[3]))
-
+    
 # 'status' is supported by all Minecraft servers that are version 1.7 or higher.
 # get server motd
-status = server.status()
-# parse 
-if sys.argv[1] == "getOnline":
-    print(status.players.online)
-elif sys.argv[1] == "getLatency":
-    print(status.latency)
-else:
-    print("{0};{1}".format(status.players.online, status.latency))
+try:
+    status = server.status()
+execpt:
+    print(0)
+    
+if status:
+    # parse 
+    if sys.argv[1] == "getOnline":
+        print(status.players.online)
+    elif sys.argv[1] == "getLatency":
+        print(status.latency)
+    else:
+        print("{0};{1}".format(status.players.online, status.latency))
