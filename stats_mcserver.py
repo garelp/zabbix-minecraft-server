03d49bf6af491cc793e333b915ba40ac51c026d1
Timo <Timobrunn@gmail.com>
2022-05-15 11:33:41 +0200

    Update stats_mcserver.py
    Fix so that old and new mcstatus is supported

--- stats_mcserver.py
+++ stats_mcserver.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python
 
-from mcstatus import JavaServer
 import sys
 
 ###sys.argv[1] - check type
 ###sys.argv[2] - ip address
 ###sys.argv[3] - port
 
+
 # Get server instance
-server = JavaServer(sys.argv[2], int(sys.argv[3]))
-    
+try:
+    from mcstatus import JavaServer
+    server = JavaServer(sys.argv[2], int(sys.argv[3]))
+except:
+    from mcstatus import MinecraftServer
+    server = MinecraftServer(sys.argv[2], int(sys.argv[3]))
+
+###sys.argv[1] - check type
+###sys.argv[2] - ip address
+###sys.argv[3] - port
+
+
 # 'status' is supported by all Minecraft servers that are version 1.7 or higher.
 # get server motd
 try:
     status = server.status()
-    # parse 
+    # parse
     if sys.argv[1] == "getOnline":
         print(status.players.online)
     elif sys.argv[1] == "getLatency":
